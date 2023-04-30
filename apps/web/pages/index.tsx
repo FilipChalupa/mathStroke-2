fdfb3d9ff2e07bc87f2e7ab32a15b8aeafa5f15a
Filip Chalupa <chalupa.filip@gmail.com>
2023-04-30 13:06:09 +0200

    Rooms: fix listener added too late

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,17 +1,44 @@
 import { Typography } from '@mui/material'
 import { ServerRooms } from 'messages'
-import { FunctionComponent, useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useMirrorLoading } from 'shared-loading-indicator'
 import { assertNever } from 'utilities'
 import { Rooms, RoomsProps } from '../components/Rooms'
 import { RoomsConnection, createRoomsConnection } from '../utilities/connection'
 
 export default function Web() {
+	const [rooms, setRooms] = useState<RoomsProps['rooms']>([])
 	const [connection, setConnection] = useState<RoomsConnection | null>(null)
+
+	const handleRequestNewRoom = useCallback(async () => {
+		// @TODO: add null check
+
+		connection.action('requestNewRoom', {
+			name: 'New Room ' + Math.round(Math.random() * 1000),
+		})
+		await new Promise((resolve) => setTimeout(resolve, 1000)) // @TODO: await new room information
+	}, [connection])
 
 	useEffect(() => {
 		const connection = createRoomsConnection(() => {
 			setConnection(connection)
+
+			const handleMessage = (message: ServerRooms.AnyMessage) => {
+				if (message.type === 'addRoomAnnouncement') {
+					setRooms((rooms) => [
+						...rooms,
+						{
+							id: message.id,
+							name: message.name,
+						},
+					])
+				} else if (message.type === 'removeRoomAnnouncement') {
+					setRooms((rooms) => rooms.filter((room) => room.id !== message.id))
+				} else {
+					assertNever(message)
+				}
+			}
+			connection.addMessageListener(handleMessage)
 		})
 
 		return () => {
@@ -27,45 +54,9 @@ export default function Web() {
 			<Typography variant="h2" component="h1">
 				Web
 			</Typography>
-			{connection && <In connection={connection} />}
+			{connection && (
+				<Rooms onRequestNewRoom={handleRequestNewRoom} rooms={rooms} />
+			)}
 		</>
 	)
-}
-
-const In: FunctionComponent<{ connection: RoomsConnection }> = ({
-	connection,
-}) => {
-	const [rooms, setRooms] = useState<RoomsProps['rooms']>([])
-
-	useEffect(() => {
-		const handleMessage = (message: ServerRooms.AnyMessage) => {
-			if (message.type === 'addRoomAnnouncement') {
-				setRooms((rooms) => [
-					...rooms,
-					{
-						id: message.id,
-						name: message.name,
-					},
-				])
-			} else if (message.type === 'removeRoomAnnouncement') {
-				setRooms((rooms) => rooms.filter((room) => room.id !== message.id))
-			} else {
-				assertNever(message)
-			}
-		}
-		connection.addMessageListener(handleMessage)
-
-		return () => {
-			connection.removeMessageListener(handleMessage)
-		}
-	}, [connection])
-
-	const handleRequestNewRoom = useCallback(async () => {
-		connection.action('requestNewRoom', {
-			name: 'New Room ' + Math.round(Math.random() * 1000),
-		})
-		await new Promise((resolve) => setTimeout(resolve, 1000)) // @TODO: await new room information
-	}, [connection])
-
-	return <Rooms onRequestNewRoom={handleRequestNewRoom} rooms={rooms} />
 }
