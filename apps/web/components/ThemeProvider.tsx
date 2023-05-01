580f8e54c07d873f8aff146bf5c4ad5b5d00f536
Filip Chalupa <chalupa.filip@gmail.com>
2023-05-01 23:25:52 +0200

    Add dark theme

---
+++ apps/web/components/ThemeProvider.tsx
@@ -0,0 +1,28 @@
+import {
+	ThemeProvider as MuiThemeProvider,
+	createTheme,
+	useMediaQuery,
+} from '@mui/material'
+import { blue } from '@mui/material/colors'
+import { ReactNode, useMemo } from 'react'
+
+export const primaryColor = blue
+export const themeColor = primaryColor[500]
+
+export const ThemeProvider: React.FunctionComponent<{
+	children: ReactNode
+}> = ({ children }) => {
+	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', )
+	const theme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode: prefersDarkMode ? 'dark' : 'light',
+					primary: primaryColor,
+				},
+			}),
+		[prefersDarkMode],
+	)
+
+	return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+}
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from '@mui/material'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import {
@@ -5,16 +6,22 @@ import {
 	SharedProgressLoadingIndicator,
 } from 'shared-loading-indicator'
 import { PageNavigationLoadingTracker } from '../components/PageNavigationLoadingTracker'
+import { ThemeProvider } from '../components/ThemeProvider'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<SharedLoadingIndicatorContextProvider>
-			<Head>
-				<title>mathStroke</title>
-			</Head>
-			<PageNavigationLoadingTracker />
-			<SharedProgressLoadingIndicator />
-			<Component {...pageProps} />
-		</SharedLoadingIndicatorContextProvider>
+		<ThemeProvider>
+			<CssBaseline />
+			<SharedLoadingIndicatorContextProvider>
+				<Head>
+					<title>mathStroke</title>
+					{/* @TODO: fix white flash in dark theme */}
+					<meta name="color-scheme" content="light dark" />{' '}
+				</Head>
+				<PageNavigationLoadingTracker />
+				<SharedProgressLoadingIndicator />
+				<Component {...pageProps} />
+			</SharedLoadingIndicatorContextProvider>
+		</ThemeProvider>
 	)
 }
