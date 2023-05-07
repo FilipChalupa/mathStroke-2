c37547358ae3bd10091fd596174015597832bf77
Filip Chalupa <chalupa.filip@gmail.com>
2023-05-07 15:19:12 +0200

    Add green color

--- packages/utilities/colors.ts
+++ packages/utilities/colors.ts
@@ -1,4 +1,4 @@
-export const colors = ['red', 'blue'] as const
+export const colors = ['red', 'green', 'blue'] as const
 
 export const defaultColor = colors[0]
 export type Color = (typeof colors)[number]
