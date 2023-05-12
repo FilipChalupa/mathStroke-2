c08dae276c86d4eba5fb62e1edb786e0ccaf03f6
Filip Chalupa <chalupa.filip@gmail.com>
2023-05-12 13:47:54 +0200

    Use `nextjs-webmanifest` package

--- apps/web/pages/app.webmanifest.ts
+++ apps/web/pages/app.webmanifest.ts
@@ -1,44 +1,29 @@
-import type { GetServerSideProps } from 'next'
-import type { WebAppManifest } from 'web-app-manifest'
+import WebmanifestPage, { createGetServerSideProps } from 'nextjs-webmanifest'
 
-function Webmanifest() {
-	// getServerSideProps will do the heavy lifting
-}
+export default WebmanifestPage
 
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-	const data: WebAppManifest = {
-		name: 'mathStroke',
-		short_name: 'mathStroke',
-		description: 'Practise your math skills.',
-		start_url: '/',
-		display: 'standalone',
-		theme_color: '#2196f3',
-		background_color: '#000000',
-		lang: 'en',
-		// @ts-ignore Id is not in the official WebAppManifest type, but it is in the specification.
-		id: 'en',
-		icons: [
-			{
-				src: '/app-icon.svg',
-				sizes: '256x256',
-				type: 'image/svg+xml',
-				purpose: 'any',
-			},
-			{
-				src: '/app-icon.svg',
-				sizes: '256x256',
-				type: 'image/svg+xml',
-				purpose: 'maskable',
-			},
-		],
-	}
-	res.setHeader('Content-Type', 'application/manifest+json')
-	res.write(JSON.stringify(data))
-	res.end()
-
-	return {
-		props: {},
-	}
-}
-
-export default Webmanifest
+export const getServerSideProps = createGetServerSideProps({
+	name: 'mathStroke',
+	short_name: 'mathStroke',
+	description: 'Practise your math skills.',
+	start_url: '/',
+	display: 'standalone',
+	theme_color: '#2196f3',
+	background_color: '#000000',
+	lang: 'en',
+	id: 'en',
+	icons: [
+		{
+			src: '/app-icon.svg',
+			sizes: '256x256',
+			type: 'image/svg+xml',
+			purpose: 'any',
+		},
+		{
+			src: '/app-icon.svg',
+			sizes: '256x256',
+			type: 'image/svg+xml',
+			purpose: 'maskable',
+		},
+	],
+})
