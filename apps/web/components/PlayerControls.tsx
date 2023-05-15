8c5380a8108d0ac7015e50180cf9cfa7f735a1c2
Filip Chalupa <chalupa.filip@gmail.com>
2023-05-15 23:06:03 +0200

    Add virtual keyboard

--- apps/web/components/PlayerControls.tsx
+++ apps/web/components/PlayerControls.tsx
@@ -1,5 +1,10 @@
 import { Button, Container, Grid, TextField } from '@mui/material'
-import { FunctionComponent, useState } from 'react'
+import {
+	FunctionComponent,
+	MouseEventHandler,
+	useCallback,
+	useState,
+} from 'react'
 
 export interface PlayerControlsProps {
 	onSolution: (solution: string) => void
@@ -12,17 +17,29 @@ export const PlayerControls: FunctionComponent<PlayerControlsProps> = ({
 }) => {
 	const [input, setInput] = useState('')
 
+	const handleNumberClick = useCallback((value: number) => {
+		setInput((input) => input + value.toString())
+	}, [])
+	const handleBackspaceClick = useCallback<
+		MouseEventHandler<HTMLButtonElement>
+	>((event) => {
+		setInput((input) => input.substring(0, input.length - 1))
+	}, [])
+
 	return (
-		<Container maxWidth="xs">
+		<Container maxWidth="xs" disableGutters>
 			<form
 				onSubmit={async (event) => {
 					event.preventDefault()
+					if (input.length === 0) {
+						return
+					}
 					onSolution(input)
 					setInput('')
 				}}
 			>
-				<Grid container gap={1}>
-					<Grid item flexGrow={1}>
+				<Grid container spacing={1}>
+					<Grid item xs={8}>
 						<TextField
 							size="small"
 							label="Your solution"
@@ -41,13 +58,35 @@ export const PlayerControls: FunctionComponent<PlayerControlsProps> = ({
 							}}
 						/>
 					</Grid>
-					<Grid item>
+					<Grid item xs={4}>
 						<Button
 							variant="contained"
 							type="submit"
+							fullWidth
 							disabled={waitingForHitConfirmation}
 						>
 							Fire
+						</Button>
+					</Grid>
+					<NumberButton value={1} onClick={handleNumberClick} />
+					<NumberButton value={2} onClick={handleNumberClick} />
+					<NumberButton value={3} onClick={handleNumberClick} />
+					<NumberButton value={4} onClick={handleNumberClick} />
+					<NumberButton value={5} onClick={handleNumberClick} />
+					<NumberButton value={6} onClick={handleNumberClick} />
+					<NumberButton value={7} onClick={handleNumberClick} />
+					<NumberButton value={8} onClick={handleNumberClick} />
+					<NumberButton value={9} onClick={handleNumberClick} />
+					<Grid item xs={4} />
+					<NumberButton value={0} onClick={handleNumberClick} />
+					<Grid item xs={4}>
+						<Button
+							variant="contained"
+							type="button"
+							fullWidth
+							onClick={handleBackspaceClick}
+						>
+							⬅
 						</Button>
 					</Grid>
 				</Grid>
@@ -55,3 +94,21 @@ export const PlayerControls: FunctionComponent<PlayerControlsProps> = ({
 		</Container>
 	)
 }
+
+const NumberButton: FunctionComponent<{
+	value: number
+	onClick: (value: number) => void
+}> = ({ value, onClick }) => (
+	<Grid item xs={4}>
+		<Button
+			variant="contained"
+			type="button"
+			fullWidth
+			onClick={() => {
+				onClick(value)
+			}}
+		>
+			{value}
+		</Button>
+	</Grid>
+)
